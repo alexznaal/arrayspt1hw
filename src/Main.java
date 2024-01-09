047743494f3d6823daadc027c03c3a75ab33c456
Alex <alxsandin@gmail.com>
2024-01-09 23:18:02 +0300

    task1-2 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task1
+        System.out.println("task1");
+        int[] array = new int[]{1, 2, 3};
+        double[] arrayDouble = new double[]{1.57, 7.654, 9.986};
+        int[] arrayRandom = new int[2];
+        arrayRandom[0] = 2;
+        arrayRandom[1] = 3;
+
+
+        //task2
+        System.out.println("task2");
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < arrayDouble.length; i++) {
+            System.out.print(arrayDouble[i] + " ");
+        }
+        System.out.println();
+
+        for (int i = 0; i < arrayRandom.length; i++) {
+            System.out.print(arrayRandom[i] + " ");
+
+        }
+
+        //task3
+        System.out.println("task3");
+
+
     }
 }
